a813d1dcb8f222de5740ef364b8fc8959f417cf1
Omargo9 <gargouri.omar123@gmail.com>
2018-12-02 02:55:23 +0100

    Updates

---
+++ precache-manifest.cf20ee9907398c9f7184a695adff93b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/movie-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2a4a27000ba76122af42",
+    "url": "/movie-app/static/js/runtime~main.2a4a2700.js"
+  },
+  {
+    "revision": "0b92ee0a549686969d63",
+    "url": "/movie-app/static/js/main.0b92ee0a.chunk.js"
+  },
+  {
+    "revision": "65ce664db22ebc0891b6",
+    "url": "/movie-app/static/js/1.65ce664d.chunk.js"
+  },
+  {
+    "revision": "0b92ee0a549686969d63",
+    "url": "/movie-app/static/css/main.3bf12831.chunk.css"
+  },
+  {
+    "revision": "58f2e40aef92d2e5e81a33102cf21dd4",
+    "url": "/movie-app/index.html"
+  }
+];
